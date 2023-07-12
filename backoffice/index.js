989517b73874c8cf0f119b82a5d495a62a92fabc
Altamir Caetano de Oliveira <altcaeol@gmail.com>
2023-07-12 15:11:42 -0300

    Arquivos Iniciais do Projeto

---
+++ backoffice/index.js
@@ -0,0 +1,8 @@
+const btn_menuPrincipal=document.querySelector("#btn_menuPrincipal");
+const menuPrincipal=document.querySelector("#menuPrincipal");
+const todosmenusprincipais=[...document.querySelectorAll(".btn_menuItem")];
+
+btn_menuPrincipal.addEventListener("click",(evt) => {
+    menuPrincipal.classList.toggle("ocultar")
+});
+
